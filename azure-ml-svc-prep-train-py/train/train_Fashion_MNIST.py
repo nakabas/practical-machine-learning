c51ddfbbbdbe534a3133be1858a009c26ab4c595
nakabas <157287013+nakabas@users.noreply.github.com>
2023-07-28 08:26:56 +0000

    zeppelin autocommit

---
+++ azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
@@ -0,0 +1,17 @@
+
+from __future__ import print_function
+import warnings
+warnings.filterwarnings('ignore')
+
+import os
+import numpy as np
+from functools import partial
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras import backend as K
