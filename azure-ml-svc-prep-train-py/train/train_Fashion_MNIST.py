990db1b2a626fc128f439e18074b48b819b271cb
nakabas <157287013+nakabas@users.noreply.github.com>
2023-07-29 21:07:01 +0000

    update to data-science

--- azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
+++ azure-ml-svc-prep-train-py/train/train_Fashion_MNIST.py
@@ -79,3 +79,18 @@ model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+# Take a look at the model summary
+model.summary()
+
+#   define compile to minimize categorical loss, use ada delta optimized, and optimize to maximizing accuracy
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(),
+              metrics=['accuracy'])
