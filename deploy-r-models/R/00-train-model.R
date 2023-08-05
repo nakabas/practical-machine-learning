8989183bf86621b7f057775b1be7d88ca19f78c1
nakabas <157287013+nakabas@users.noreply.github.com>
2023-08-05 18:42:39 +0000

    updated readme.txt to keep it up to date

--- deploy-r-models/R/00-train-model.R
+++ deploy-r-models/R/00-train-model.R
@@ -2,4 +2,5 @@
 #   Create, Test and Save a Logistic Regression Model    #
 ##########################################################
 
-# Use logistic regression equation of vehicle 
+# Use logistic regression equation of vehicle transmission 
+# in th
