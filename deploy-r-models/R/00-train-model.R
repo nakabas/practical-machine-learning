d1cc70ceb904f5a56a54514128aec80428719272
nakabas <157287013+nakabas@users.noreply.github.com>
2023-08-07 19:07:58 +0000

    Fix email address.

--- deploy-r-models/R/00-train-model.R
+++ deploy-r-models/R/00-train-model.R
@@ -5,3 +5,4 @@
 # Use logistic regression equation of vehicle transmission 
 # in the data set mtcars to estimate the probability of 
 # a vehicle being fitted with a manual transmission 
+# based on
