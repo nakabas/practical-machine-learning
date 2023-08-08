060e33c2a45619cb7f67f936865526706b5b9f3d
nakabas <157287013+nakabas@users.noreply.github.com>
2023-08-08 06:57:20 +0000

    Update resource.md

--- deploy-r-models/R/00-train-model.R
+++ deploy-r-models/R/00-train-model.R
@@ -5,4 +5,4 @@
 # Use logistic regression equation of vehicle transmission 
 # in the data set mtcars to estimate the probability of 
 # a vehicle being fitted with a manual transmission 
-# based on horsepower (hp
+# based on horsepower (hp) and weight (wt
