ba386dadb991b941e37a5f67311b17168ff98f1f
nakabas <157287013+nakabas@users.noreply.github.com>
2023-08-22 23:07:19 +0000

    Add test code.

--- deploy-r-models/R/02b-deploy-aci.sh
+++ deploy-r-models/R/02b-deploy-aci.sh
@@ -2,3 +2,15 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
+
+# -e: immediately exit if any command has a non-zero exit status
+# -o: prevents errors in a pipeline from being masked
+# IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
+
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation>" 1>&2; exit 1; }
+
+declare subscriptionId=""
+declare resourceGroupName=""
+declare deploymentName="car-svc-aci-`date '+%Y-%m-%d-%H-%M-%S'`"
+declare resourceGroupLocation=""
+declare aciDnsNameLabel=""
