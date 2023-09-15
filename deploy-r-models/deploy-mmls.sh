42b4bdf88ba43e7c0d8c4ff5318d0693e88fb6a1
nakabas <157287013+nakabas@users.noreply.github.com>
2023-09-15 07:13:46 +0000

    login update

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -5,3 +5,5 @@ IFS=$'\n\t'
 # -e: immediately exit if any command has a non-zero exit status
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
+
+usage() { 
