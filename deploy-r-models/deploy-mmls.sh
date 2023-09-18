778694e12446d633fcf7085ce584629b7f85df61
nakabas <157287013+nakabas@users.noreply.github.com>
2023-09-18 06:36:43 +0000

    README edits.

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -6,4 +6,4 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <r
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation> -v
