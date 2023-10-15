5398e5dc8aa4a603c97030485a3d9aca9f593d06
nakabas <157287013+nakabas@users.noreply.github.com>
2023-10-15 13:27:33 +0000

    Update codelation_ui.gemspec

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -47,4 +47,5 @@ shift $((OPTIND-1))
 # Requirements check: jq
 command -v jq >/dev/null 2>&1 || { echo >&2 "jq is required by this script but it's not installed. Please check https://stedolan.github.io/jq/download/ for details how to install jq."; exit 1; }
 
-#Prompt for parameters is some required parameters are missi
+#Prompt for parameters is some required parameters are missing
+if [[ -z "$subscript
