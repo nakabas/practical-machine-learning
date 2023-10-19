d411b841a84f1823ad5bd8cd4fb2eb3c480492f4
nakabas <157287013+nakabas@users.noreply.github.com>
2023-10-19 08:57:37 +0000

    Tidyups

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -51,4 +51,5 @@ command -v jq >/dev/null 2>&1 || { echo >&2 "jq is required by this script but i
 if [[ -z "$subscriptionId" ]]; then
 	echo "Your subscription ID can be looked up with the CLI using: az account show --out json "
 	echo "Enter your subscription ID:"
-	read subscription
+	read subscriptionId
+	[[ "${subscrip
