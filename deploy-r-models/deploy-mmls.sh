3e757872e9c34128033f16cb3e81fb76f5663cd1
nakabas <157287013+nakabas@users.noreply.github.com>
2023-10-23 09:14:54 +0000

    fixed logout

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -56,4 +56,5 @@ if [[ -z "$subscriptionId" ]]; then
 fi
 
 if [[ -z "$resourceGroupName" ]]; then
-	echo "This script will look for an existing resource group, otherwise a new one will b
+	echo "This script will look for an existing resource group, otherwise a new one will be created "
+	
