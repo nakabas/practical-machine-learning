7fcdf73e033a652dbcef53cdddc4a657c854949b
nakabas <157287013+nakabas@users.noreply.github.com>
2023-10-27 08:49:35 +0000

    Use short array syntax

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -59,4 +59,5 @@ if [[ -z "$resourceGroupName" ]]; then
 	echo "This script will look for an existing resource group, otherwise a new one will be created "
 	echo "You can create new resource groups with the CLI using: az group create "
 	echo "Enter a resource group name: "
-	read res
+	read resourceGroupName
+	[[ "
