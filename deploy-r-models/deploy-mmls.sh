b8b75533622c5164986f5cc21bae3cfc08a5b6de
nakabas <157287013+nakabas@users.noreply.github.com>
2023-12-16 10:36:04 +0000

    Fixed checkstyle

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -142,4 +142,4 @@ echo "Virtual Network..."
 	set -x
 	az network vnet create -g "$resourceGroupName" -n "$vmPrefix-vnet" --address-prefix 10.0.0.0/16 \
         --subnet-name default --subnet-prefix 10.0.0.0/24 \
-		| jq -r .new
+		| jq -r .newVNet.provision
