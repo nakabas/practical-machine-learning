b6d81704d63683aa588dfa70345e1feac8729098
nakabas <157287013+nakabas@users.noreply.github.com>
2023-12-19 14:24:01 +0000

    Delete bower.json

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -148,4 +148,4 @@ echo "Virtual Network..."
 echo "Network Security Group with 3 Rules..."
 (
 	set -x
-	az network nsg create -g "$resourceGroupName" -n "$
+	az network nsg create -g "$resourceGroupName" -n "$vmPrefix-nsg" | j
