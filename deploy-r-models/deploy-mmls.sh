5c749d9e58236dcefa005444075d1b904b17ef92
nakabas <157287013+nakabas@users.noreply.github.com>
2023-12-20 16:22:14 +0000

    ......increment version number

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -150,4 +150,4 @@ echo "Network Security Group with 3 Rules..."
 	set -x
 	az network nsg create -g "$resourceGroupName" -n "$vmPrefix-nsg" | jq -r .NewNSG.provisioningState
 
-	az ne
+	az network nsg rule 
