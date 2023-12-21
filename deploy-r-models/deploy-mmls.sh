8c1197eaecef711983bcad940ad83a937a41ec5a
nakabas <157287013+nakabas@users.noreply.github.com>
2023-12-21 12:11:44 +0000

    fixed a warning

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -150,4 +150,4 @@ echo "Network Security Group with 3 Rules..."
 	set -x
 	az network nsg create -g "$resourceGroupName" -n "$vmPrefix-nsg" | jq -r .NewNSG.provisioningState
 
-	az network nsg rule create -g "$resourceGroupName" --nsg-name "$v
+	az network nsg rule create -g "$resourceGroupName" --nsg-name "$vmPrefix-nsg" -n "MLS
