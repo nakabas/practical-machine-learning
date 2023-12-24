c516c23d769c973dce81da178e6741e3b3e43b51
nakabas <157287013+nakabas@users.noreply.github.com>
2023-12-24 22:52:25 +0000

    Updated dependencies to latest; added ncu

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -151,4 +151,5 @@ echo "Network Security Group with 3 Rules..."
 	az network nsg create -g "$resourceGroupName" -n "$vmPrefix-nsg" | jq -r .NewNSG.provisioningState
 
 	az network nsg rule create -g "$resourceGroupName" --nsg-name "$vmPrefix-nsg" -n "MLSvr_WebNode" \
-		--priority 1000 --access Allow --protocol Tcp --dir
+		--priority 1000 --access Allow --protocol Tcp --direction Inbound \
+		--d
