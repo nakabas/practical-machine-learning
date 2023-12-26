3bdf9d078938647bce5aadffd28d52bd494b1877
nakabas <157287013+nakabas@users.noreply.github.com>
2023-12-26 17:56:08 +0000

    :memo: Update readme

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -153,4 +153,6 @@ echo "Network Security Group with 3 Rules..."
 	az network nsg rule create -g "$resourceGroupName" --nsg-name "$vmPrefix-nsg" -n "MLSvr_WebNode" \
 		--priority 1000 --access Allow --protocol Tcp --direction Inbound \
 		--destination-address-prefixes '*' --destination-port-ranges 12800 \
-		| jq -r .provisioning
+		| jq -r .provisioningState
+
+	az network
