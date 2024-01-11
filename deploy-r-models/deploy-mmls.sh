5fed0356d0de61b95ce91ce0d08c44ac2f33e57a
nakabas <157287013+nakabas@users.noreply.github.com>
2024-01-11 22:15:52 +0000

    requirements

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -171,4 +171,4 @@ echo "Public IP & NIC..."
 	set -x
 	az network public-ip create -g "$resourceGroupName" -n "$vmPrefix-ip" --sku Basic \
 		| jq -r .publicIp.provisioningState
-	az network nic c
+	az network nic create -g "$r
