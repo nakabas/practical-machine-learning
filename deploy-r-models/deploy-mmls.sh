da1be1a412c78a3eb69ce21cdf42e804c94506c5
nakabas <157287013+nakabas@users.noreply.github.com>
2024-01-12 20:22:45 +0000

    Fix badge link

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -171,4 +171,4 @@ echo "Public IP & NIC..."
 	set -x
 	az network public-ip create -g "$resourceGroupName" -n "$vmPrefix-ip" --sku Basic \
 		| jq -r .publicIp.provisioningState
-	az network nic create -g "$resourceGrou
+	az network nic create -g "$resourceGroupName" -n "$vmPrefi
