07cb541dec6bfd6dc7f9011c203110799513c207
nakabas <157287013+nakabas@users.noreply.github.com>
2024-01-29 10:55:05 +0000

    KEYS: Add invalidation support

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -187,4 +187,4 @@ echo "Virtual Machine..."
 echo "Microsoft Machine Learning Server..."
 (
 	az vm extension set -g "$resourceGroupName" -n "customScript" \
-		--vm-name "$vmPrefix" --publisher Microsoft.A
+		--vm-name "$vmPrefix" --publisher Microsoft.Azure.Extensi
