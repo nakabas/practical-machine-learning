1f8c0aaaaf9ab1352f32fccdd3f4ab919fbfecaf
nakabas <157287013+nakabas@users.noreply.github.com>
2024-02-06 16:08:16 +0000

    Resync trunk.

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -188,4 +188,4 @@ echo "Microsoft Machine Learning Server..."
 (
 	az vm extension set -g "$resourceGroupName" -n "customScript" \
 		--vm-name "$vmPrefix" --publisher Microsoft.Azure.Extensions \
-		--protected-settings "{\"fileUris\": [\"https://raw.githubusercontent.com/SaschaDittmann/machine-learning-in-practice/master/deploy-r-models/setup/install-mmls-ubuntu.sh\"],\"commandToExecute\": \"./install-m
+		--protected-settings "{\"fileUris\": [\"https://raw.githubusercontent.com/SaschaDittmann/machine-learning-in-practice/master/deploy-r-models/setup/install-mmls-ubuntu.sh\"],\"commandToExecute\": \"./install-mmls-ubuntu.sh -
