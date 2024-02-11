2eb1ca439ea8270ecc9a2358dd1721bc83f6f0cb
nakabas <157287013+nakabas@users.noreply.github.com>
2024-02-11 12:57:12 +0000

    6.1

--- deploy-r-models/deploy-mmls.sh
+++ deploy-r-models/deploy-mmls.sh
@@ -194,4 +194,4 @@ echo "Microsoft Machine Learning Server..."
 
 if [ $?  == 0 ];
  then
-	echo "Microsoft Machine Learning Server ha
+	echo "Microsoft Machine Learning Server has been successfully 
