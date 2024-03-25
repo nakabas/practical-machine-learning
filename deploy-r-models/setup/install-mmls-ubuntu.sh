cd78e5c38dd6f721c099f3fbd62b630bdb5baa83
nakabas <157287013+nakabas@users.noreply.github.com>
2024-03-25 14:03:43 +0000

    cpufreq: intel_pstate: Fix setting max_perf_pct in performance policy

--- deploy-r-models/setup/install-mmls-ubuntu.sh
+++ deploy-r-models/setup/install-mmls-ubuntu.sh
@@ -46,4 +46,6 @@ dpkg -i packages-microsoft-prod.deb
 rm -rf packages-microsoft-prod.deb
 
 # Add the Microsoft public signing key for Secure APT
-apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C
+apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+
+# Upda
