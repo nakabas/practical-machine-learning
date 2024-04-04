25c1fdacce956266b056ba0ccecd4915bc95388a
nakabas <157287013+nakabas@users.noreply.github.com>
2024-04-04 05:25:02 +0000

    Update cloudbuild.yaml

--- deploy-r-models/setup/install-mmls-ubuntu.sh
+++ deploy-r-models/setup/install-mmls-ubuntu.sh
@@ -58,4 +58,4 @@ apt-get install -y microsoft-mlserver-all-9.4.7
 /opt/microsoft/mlserver/9.4.7/bin/R/activate.sh -a -l
 
 # Set up both o16n nodes on one machine
-az mlserver admin node setup --onebox --admin-password $password --confirm-p
+az mlserver admin node setup --onebox --admin-password $password --confirm-password $password
